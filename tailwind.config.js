48448c130e7e96bbdfa4f127549550d13e77fba4
masihgh <masihprogrammer@gmail.com>
2023-05-25 20:52:00 +0330

    config daysi ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,38 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    styled: true,
+    themes: true,
+    prefix: "",
+    darkTheme: "darkChat",
+      themes: [
+        {
+          Theme: {
+			 "primary": "#48352d",
+			 "secondary": "#065f46",
+			 "accent": "#b45309",
+			 "neutral": "#48352d",
+			 "base-100": "#FFFFFF",
+			 "info": "#38bdf8",
+			 "success": "#10b981",
+			 "warning": "#eab308",
+			 "error": "#ef4444",
+          },
+          darkChat: {
+			 "primary": "#48352d",
+			 "secondary": "#065f46",
+			 "accent": "#b45309",
+			 "neutral": "#48352d",
+			 "base-100": "#FFFFFF",
+			 "info": "#38bdf8",
+			 "success": "#10b981",
+			 "warning": "#eab308",
+			 "error": "#ef4444",
+          },
+        },
+      ],
+  },
   plugins: [require("daisyui")],
 
 }
