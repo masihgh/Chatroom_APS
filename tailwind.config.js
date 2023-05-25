a314de1a987ede10ade69f5629d3aecf42a1a95b
ps220455 <ps220455@summacollege.nl>
2023-05-25 19:23:37 +0200

    dark light chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
@@ -11,34 +9,32 @@ export default {
     themes: true,
     prefix: "",
     darkTheme: "darkChat",
-      themes: [
-        {
-          Theme: {
-			 "primary": "#48352d",
-			 "secondary": "#065f46",
-			 "accent": "#b45309",
-			 "neutral": "#48352d",
-			 "base-100": "#FFFFFF",
-			 "info": "#38bdf8",
-			 "success": "#10b981",
-			 "warning": "#eab308",
-			 "error": "#ef4444",
-          },
-          darkChat: {
-			 "primary": "#48352d",
-			 "secondary": "#065f46",
-			 "accent": "#b45309",
-			 "neutral": "#48352d",
-			 "base-100": "#FFFFFF",
-			 "info": "#38bdf8",
-			 "success": "#10b981",
-			 "warning": "#eab308",
-			 "error": "#ef4444",
-          },
+    themes: [
+      {
+        lightChat: {
+          primary: "#48352d",
+          secondary: "#065f46",
+          accent: "#b45309",
+          neutral: "#48352d",
+          "base-100": "#FFFFFF",
+          info: "#38bdf8",
+          success: "#10b981",
+          warning: "#eab308",
+          error: "#ef4444",
         },
-      ],
+        darkChat: {
+          primary: "#48352d",
+          secondary: "#065f46",
+          accent: "#b45309",
+          neutral: "#48352d",
+          "base-100": "#FFFFFF",
+          info: "#38bdf8",
+          success: "#10b981",
+          warning: "#eab308",
+          error: "#ef4444",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
-
-}
-
+};
