f9a92af8a89c679b951a2c5c2f8133beae758b14
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2023-05-26 08:54:34 +0330

    create navbar component and import it in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
+      <h1 className="bg-rose-500 ">Chat app</h1>
+      <h2>Sahar</h2>
 
-     <h1 className='bg-rose-500 '>Chat app</h1>
-     <h2>Sahar</h2>
-
-     <h1 className='bg-rose-500 '>Chat app hello</h1>
-     <h2>kani</h2>
-
+      <h1 className="bg-rose-500 ">Chat app hello</h1>
+      <h2>kani</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
