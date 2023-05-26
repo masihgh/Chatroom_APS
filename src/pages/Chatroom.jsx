08577481a6b10d8183f80b285f7c75dd737acb87
ps220455 <ps220455@summacollege.nl>
2023-05-26 22:14:07 +0200

    add style of page for full screen

--- src/pages/Chatroom.jsx
+++ src/pages/Chatroom.jsx
@@ -3,11 +3,11 @@ const Chatroom = () => {
     <div className="bg-accent-content w-full h-screen">
       {/* chatroom section */}
       <section className="px-40">
-      {/* Message sender */}
+        {/* Message sender */}
         <div className="chat chat-start">
           <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" />
             </div>
           </div>
           <div className="chat-header">
@@ -21,7 +21,7 @@ const Chatroom = () => {
         <div className="chat chat-end">
           <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" />
             </div>
           </div>
           <div className="chat-header">
@@ -32,6 +32,21 @@ const Chatroom = () => {
           <div className="chat-footer opacity-50">Seen at 12:46</div>
         </div>
       </section>
+      {/* send message input */}
+      <div className="bg-primary h-1/5 w-full absolute bottom-0 ">
+        <div className="form-control py-12 px-32">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="type..."
+              className="input input-bordered w-full"
+            />
+            <button className="btn bg-primary-content text-primary">
+              send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
