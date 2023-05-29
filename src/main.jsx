786bcffefda41c44cfa8c9ca4ebb95340a9c4ce5
ps220455 <ps220455@summacollege.nl>
2023-05-29 18:59:17 +0200

    END

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/index.css";
