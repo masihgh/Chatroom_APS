b919c2fc34eb4d4c96bb37931b33101058afb681
ps220455 <ps220455@summacollege.nl>
2023-05-29 18:09:05 +0200

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,39 @@ import App from "./App.jsx";
 import "./assets/index.css";
 const AuthContext = createContext();
 
+const [currentUser, setCurrentUser] = useState(null);
+const [loading, setLoading] = useState(true);
+
+// signin with google
+const signinWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+};
+
+// signout
+const logout = () => signOut(auth);
+
+// set currentUser
+useEffect(() => {
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+    setCurrentUser(user);
+    setLoading(false);
+  });
+
+  return unsubscribe;
+}, []);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthContext.Provider>
+    <AuthContext.Provider
+      value={{
+        unsubscribe,
+        currentUser,
+        setCurrentUser,
+        signinWithGoogle,
+        logout,
+      }}
+    >
       <App />
     </AuthContext.Provider>
   </React.StrictMode>
