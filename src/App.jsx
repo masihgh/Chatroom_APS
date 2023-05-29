726838960147668b2b7aeca7735356197e4e42c3
ps220455 <ps220455@summacollege.nl>
2023-05-29 18:19:33 +0200

    extras

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
@@ -43,6 +44,7 @@ function App() {
           }
         />
       </Routes>
+    </>
   );
 }
 
