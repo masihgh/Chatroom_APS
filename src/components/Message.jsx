1547a63495304fefb66492aeaa00da69056f84ae
MasihGhaznavi <48735862+masihgh@users.noreply.github.com>
2023-05-29 20:09:55 +0330

    Create Message.jsx
    creating chat bubbles

---
+++ src/components/Message.jsx
@@ -0,0 +1,20 @@
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <div>
+      <div className={`chat ${message.uid === currentUser.uid ? "chat-end" : "chat-start"}`}>
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src={message.avatar} />
+          </div>
+        </div>
+        <div className="chat-header">
+          {message.name}
+        </div>
+        <div className="chat-bubble">{message.text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
