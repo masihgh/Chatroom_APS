3e8c828af2c47d2b8874b5d4261ed39e35cb45a5
ps220455 <ps220455@summacollege.nl>
2023-05-29 18:46:54 +0200

    firebases exports

---
+++ src/components/SendMessage.jsx
@@ -0,0 +1,54 @@
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useContext, useState } from "react";
+
+import { db } from "../firebase";
+import { AuthContext } from "../main";
+
+const SendMessage = () => {
+  const [value, setValue] = useState("");
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+
+    if (value.trim() === "") {
+      alert("Enter valid message!");
+      return;
+    }
+
+    try {
+      const { uid, displayName, photoURL } = currentUser;
+      await addDoc(collection(db, "messages"), {
+        text: value,
+        name: displayName,
+        avatar: photoURL,
+        createdAt: serverTimestamp(),
+        uid,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    setValue("");
+  };
+
+  return (
+    <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
+      <form onSubmit={handleSendMessage} className="px-2 containerWrap flex">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="input w-full focus:outline-none bg-gray-100 rounded-r-none"
+          type="text"
+        />
+        <button
+          type="submit"
+          className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SendMessage;
